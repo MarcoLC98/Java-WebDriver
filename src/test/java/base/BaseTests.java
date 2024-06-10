7bd0fdf39ed6494a6265036f041efbc1dfa78f6f
Marco Cannata <marcocannata76@gmail.com>
2024-06-10 16:58:23 -0300

    last chapter practice added, course finished

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,11 +1,13 @@
 package base;
 
 import com.google.common.io.Files;
+import org.checkerframework.checker.units.qual.C;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.events.WebDriverListener;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
@@ -16,7 +18,6 @@ import utils.WindowManager;
 
 import java.io.File;
 import java.io.IOException;
-import java.time.Duration;
 
 public class BaseTests {
 
@@ -26,9 +27,9 @@ public class BaseTests {
     @BeforeClass
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(getChromeOptions());
         goHome();
-
+        setCookie();
     }
 
     @BeforeMethod
@@ -59,5 +60,17 @@ public class BaseTests {
 
     public WindowManager getWindowManager() {
         return new WindowManager(driver);
+    }
+
+    private ChromeOptions getChromeOptions() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("disable-infobars");
+        options.addArguments("--headless");
+        return options;
+    }
+
+    private void setCookie() {
+        Cookie cookie = new Cookie.Builder("tau", "123").domain("https://the-internet.herokuapp.com/").build();
+        driver.manage().addCookie(cookie);
     }
 }
